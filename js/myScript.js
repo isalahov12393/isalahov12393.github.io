8255f4992d7cf316e3d18f3c4d603fdecc2e81a1
Isa Salahov <97687195+isalahov12393@users.noreply.github.com>
2022-10-15 01:02:30 +0400

    updated

--- js/myScript.js
+++ js/myScript.js
@@ -5,9 +5,6 @@ function realtimeClock() {
     var seconds = rtClock.getSeconds();
     var amPm = ( hours < 12) ? "AM" : "PM";
     hours = (hours > 12) ? hours-12 : hours;
-    hours = ("0" + hours);
-    minutes = ("0" + minutes);
-    seconds = ("0" + seconds);
     document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds + " " + amPm;
     var t = setTimeout(realtimeClock,500);
   }
